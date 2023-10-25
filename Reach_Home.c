925c4032243fa8795e28bcf277e587c18a3f7396
siripurapuswetha <147791877+siripurapuswetha@users.noreply.github.com>
2023-10-25 22:38:30 +0530

    #include<stdio.h> int main() {     int X,Y;     scanf("%d%d",&X,&Y);     if(Y<=5*X)     {         printf("YES");     }     else     {         printf("NO");     }
    }

---
+++ Reach_Home.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y;
+    scanf("%d%d",&X,&Y);
+    if(Y<=5*X)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    
+}
